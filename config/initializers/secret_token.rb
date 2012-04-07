a2453c934adec684b3d38b204804e6d6bc9abf84
Albert Peng <albert.peng@halogennetwork.com>
2012-04-07 15:21:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CiMonitor::Application.config.secret_token = '91b448bffa4aeccdc9679edec84b4ea9ef650640b9680418ca2a9c96944083680868616bce693b1795dcb9c91dcc7bf62d02108ab92474be8aa20b1efcb1db7f'
